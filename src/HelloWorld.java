64507a012ebba386c1f608b4cebef5946e65ed20
NatnaelTsige <70252612+NatnaelTsige@users.noreply.github.com>
2020-09-01 23:35:48 -0700

    main method and temp method.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,17 @@
 public class HelloWorld {
+
+    public static void temp(double faren) {
+        double celc =5 * (faren - 32) / 9;
+        System.out.println("Farenheit: " + faren + "\n Celcius: " + celc + "\n");
+    }
+
+    public static void main(String[] args){
+        System.out.println("Hello world!\n");
+
+        double f = 2.4;
+
+        temp(5.6);
+        temp(f);
+    }
+
 }
